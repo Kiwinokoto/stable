bb9fb384314608d0050a9ef15601b2103e18d282
Oudelet Kevin <kevin.oudelet@gmail.com>
2024-11-29 14:38:46 +0100

    second commit

--- generate.py
+++ generate.py
@@ -20,13 +20,11 @@ pipe.to("cpu")
 # generate + save pic
 prompt = "a majestic mountain landscape"
 prompt = "a peaceful sky full of stars"
+prompt = "a nice anime style room, with a bed, a window, pictures of friends on the walls"
 
-image = pipe(prompt, height=400, width=400).images[0] # default size 512x512
+# default size 512x512
+# default inference steps 50
+
+image = pipe(prompt, height=400, width=400, num_inference_steps=30, guidance_scale=5.0).images[0] # default guidance_scale=7.5 ?
 image.save("output.png")
-
-image = pipe(prompt, height=400, width=400, num_inference_steps=25).images[0] # default inference steps 50 ?
-image.save("output_less_steps.png")
-
-image = pipe(prompt, height=400, width=400, num_inference_steps=40, guidance_scale=5.0).images[0] # default guidance_scale=7.5 ?
-image.save("output_less_guidance.png")
 
